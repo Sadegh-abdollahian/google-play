50ee573c2dfe231583c883970832a9b22b4df4fd
Sadegh <sadeghdata1387@gmail.com>
2025-06-14 12:53:18 +0330

    finished order viewset

--- orders/api/v1/views.py
+++ orders/api/v1/views.py
@@ -1,24 +1,29 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers, response, status
 from orders.models import Order
 from .serializers import OrderSerializer
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
 
 
 class OrderViewset(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         if Order.objects.filter(
             user=self.request.user, app=serializer.validated_data.get("app")
         ).exists():
-            print(
-                Order.objects.filter(
-                    user=self.request.user, app=serializer.validated_data.get("app")
-                )
-            )
             raise PermissionDenied("You have already purchased this app.")
 
         if serializer.is_valid():
+            if (
+                serializer.validated_data.get("total_amount")
+                != serializer.validated_data.get("app").price
+            ):
+                raise PermissionDenied("Total amount is not correct.")
             serializer.save(user=self.request.user)
+
+            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return serializers.ValidationError("Serializer is not valid.")
