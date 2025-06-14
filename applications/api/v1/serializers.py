fabb4b149c003c47bf80ffeba712a02784604088
Sadegh <sadeghdata1387@gmail.com>
2025-06-14 20:22:20 +0330

    completed apps purchasing logic

--- applications/api/v1/serializers.py
+++ applications/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from applications.models import App, Category
+from orders.models import Order
 
 
 class AppSerializer(serializers.ModelSerializer):
@@ -7,6 +8,21 @@ class AppSerializer(serializers.ModelSerializer):
     class Meta:
         model = App
         fields = "__all__"
+        lookup_field = "slug"
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        request = self.context.get("request")
+        user = request.user if request else None
+
+        if instance.price == 0 or user.is_superuser:
+            return representation
+
+        if Order.objects.filter(user=user, app=instance, status="purchased").exists():
+            return representation
+
+        representation.pop("apk_file", None)
+        return representation
 
 
 class CategorySerializer(serializers.ModelSerializer):
