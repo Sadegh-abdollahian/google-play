eaad62e5be2804edf3f8c61f9308d7bc29bf7390
Sadegh <sadeghdata1387@gmail.com>
2025-07-05 15:52:43 +0330

    configured django filters and added filters for app viewset

--- applications/api/v1/filters.py
+++ applications/api/v1/filters.py
@@ -5,7 +5,28 @@ from applications.models import App
 class AppFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
     max_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+    min_legal_age = django_filters.NumberFilter(
+        field_name="legal_age", lookup_expr="gte"
+    )
+    max_legal_age = django_filters.NumberFilter(
+        field_name="legal_age", lookup_expr="lte"
+    )
+    min_downloads = django_filters.NumberFilter(
+        field_name="downloads", lookup_expr="gte"
+    )
+    max_downloads = django_filters.NumberFilter(
+        field_name="downloads", lookup_expr="lte"
+    )
+    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
 
     class Meta:
         model = App
-        fields = ["min_price", "max_price"]
+        fields = [
+            "name",
+            "min_price",
+            "max_price",
+            "min_legal_age",
+            "max_legal_age",
+            "min_downloads",
+            "max_downloads",
+        ]
