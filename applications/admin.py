1a79fc353b12870d756dd63e88c711dbb9d574bd
Sadegh <sadeghdata1387@gmail.com>
2025-07-08 23:14:22 +0330

    upgraded the app admin class

--- applications/admin.py
+++ applications/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from .models import App, Category, AppImage, Review
 
+
 # Register your models here.
-admin.site.register(App)
+@admin.register(App)
+class AppAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "subname",
+        "status",
+        "category",
+        "downloads",
+        "price",
+        "slug",
+        "tags",
+        "owner",
+        "created",
+    ]
+    list_filter = ["status", "category"]
+    search_fields = ["name", "owner"]
+    prepopulated_fields = {"slug": ("name",)}
+    readonly_fields = ["updated", "created"]
+
+
 admin.site.register(Category)
 admin.site.register(AppImage)
 admin.site.register(Review)
