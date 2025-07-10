bf4f94bcb41b217d2ffa415b10bc0cf623edb875
Sadegh <sadeghdata1387@gmail.com>
2025-07-10 18:08:30 +0330

    added order admin class

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Order
 
+
 # Register your models here.
-admin.site.register(Order)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "app",
+        "total_amount",
+        "status",
+        "created",
+        "updated",
+    ]
+    list_filter = ["status", "total_amount"]
+    search_fields = ["user", "app"]
+    prepopulated_fields = {"slug": ("name",)}
+    readonly_fields = ["updated", "created"]
